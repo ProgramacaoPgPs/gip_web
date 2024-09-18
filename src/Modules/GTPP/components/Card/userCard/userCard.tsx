3daf36156904f3f1b0c6c00e2e392ae8b7c919bc
ProgramacaoPgPs <programacaopegpese@gmail.com>
2024-09-18 12:42:29 -0300

    I'm meaking some ajusments in working file modules for working with big card information

---
+++ src/Modules/GTPP/components/Card/userCard/userCard.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Button, Collapse, Image, Card as BootstrapCard } from "react-bootstrap";
+
+type UsePropsCard = {
+    employeeName?: string;
+    employeeImage?: string;
+}
+
+const UserCard: React.FC<UsePropsCard> = (props) => {
+    const [openUser, setOpenUser] = useState(false);
+
+    return (
+        <React.Fragment>
+         <div className="col-12 mt-3 d-flex align-items-center justify-content-end position-relative">
+            <div
+                className="user-toggle-btn cursor-pointer d-block position-absolute add-user"
+                onClick={() => console.log('abrindo modal de registro para usuário')}
+                aria-controls="user-info"
+                aria-expanded={openUser}
+              >
+              <Button variant="transparent" className="icon-btn fa fa-plus" />
+              <div>
+                <span className="ms-2 employee-name">{props.employeeName}</span>
+              </div>
+            </div>
+            <div
+              className="user-toggle-btn cursor-pointer d-block"
+              onClick={() => setOpenUser(!openUser)}
+              aria-controls="user-info"
+              aria-expanded={openUser}
+            >
+              <Image 
+                src={props.employeeImage || 'https://via.placeholder.com/50'}
+                roundedCircle 
+                className="employee-img"
+                alt={props.employeeName || 'Usuário'}
+              />
+              <div>
+                <span className="ms-2 employee-name">{props.employeeName || 'User'}</span>
+              </div>
+            </div>
+          </div>
+          <Collapse in={openUser}>
+            <div id="user-info" className="col-12 mt-2">
+              <BootstrapCard className="border-dark">
+                <BootstrapCard.Body className="p-1">
+                  <div className="d-flex align-items-center gap-2">
+                    <Image 
+                      src={props.employeeImage || 'https://via.placeholder.com/50'}
+                      roundedCircle 
+                      className="employee-img"
+                    />
+                    <span className="employee-name">{props.employeeName || 'User'}</span>
+                  </div>
+                </BootstrapCard.Body>
+              </BootstrapCard>
+            </div>
+          </Collapse>
+        </React.Fragment>
+    )
+}
+
+export default UserCard;
