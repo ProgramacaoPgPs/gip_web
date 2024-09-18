cc97717a34b16dcf5066ab2c35abe8a3e2f5b1cb
ProgramacaoPgPs <programacaopegpese@gmail.com>
2024-09-18 10:37:52 -0300

    I'm to some ajusments in work file components wtih renderPage and index page Home of intranet

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -8,14 +8,14 @@ import { useNavigate } from 'react-router-dom';
 export default function Home(): JSX.Element {
     const { setIsLogged,setTitleHead } = useMyContext();
     React.useEffect(()=>{
-        setTitleHead({title:'Home - GIPP',icon:'fa fa-home'});
+        setTitleHead({title:'Home - GIPP', icon:'fa fa-home'});
     },[]);
-    const listPath = [{page: '/home', children: 'Home'}, {page: '/config', children: 'Configuração'}, {page: '/home', children: 'Sair'}];
+    const listPath = [{page: '/home', children: 'Home', icon:'fa fa-home' }, {page: '/config', children: 'Configuração', icon:'fa fa-gear'}, {page: '/', children: 'Sair', icon:'fa fa-sign-out'}];
     const navigate = useNavigate();
 
     return (
         <div className='d-flex flex-row w-100 h-100'>
-            <NavBar list={listPath}/>
+            <NavBar  list={listPath}/>
             <section className='m-2'>
                 <CustomButton onClick={() => navigate('/home/GTPP')} className='btn btn-primary' value={'GTPP'} />
             </section>
--- src/Components/Navbar/NavBar.tsx
+++ src/Components/Navbar/NavBar.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import { Container } from 'react-bootstrap';
 
 type NavBarProps = {
     list?: any[],
     page?: string, 
+    icon?: string,
     children?: React.ReactNode,
 }
 
@@ -18,20 +20,22 @@ const NavBar: React.FC<NavBarProps> = (props: any) => {
 
     return (
         <Navbar expand="" id='navGipp' className="align-items-start bg-transparent">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleClick} />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    {props.list && props.list.length > 0 ? (
-                        props.list.map((item: any, index: any) => (
-                            <Nav.Link key={index} as={Link} to={item.page || "/home"}>
-                                {item.children || "Default Text"}
-                            </Nav.Link>
-                        ))
-                    ) : (
-                        <p>No items to display</p>
-                    )}
-                </Nav>
-            </Navbar.Collapse>
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleClick} />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {props.list && props.list.length > 0 ? (
+                            props.list.map((item: any, index: any) => (
+                                <Nav.Link key={index} as={Link} to={item.page || "/home"}>
+                                   <div className={item.icon} ></div> {item.children || "Default Text"}
+                                </Nav.Link>
+                            ))
+                        ) : (
+                            <p>No items to display</p>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 };
