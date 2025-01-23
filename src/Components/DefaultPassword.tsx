85ece9063a1cbe231dedd2368ff4e9b01ad28bb1
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2025-01-23 15:29:50 -0300

    Add close in backgound - file DefaultPassword (Correction bug)

--- src/Components/DefaultPassword.tsx
+++ src/Components/DefaultPassword.tsx
@@ -10,7 +10,7 @@ export default function DefaultPassword(props: {
         login: string
         password: string
     },
-    onClose:()=>void
+    onClose: () => void
 }): JSX.Element {
     const [password, setPassword] = useState<string>("");
     const [confirm, setConfirm] = useState<string>("");
@@ -21,9 +21,9 @@ export default function DefaultPassword(props: {
             setLoading(true);
             event.preventDefault();
             if (password !== confirm) throw new Error("As senhas não conferem");
-            const connection = new Connection("15",true);
-            const req:any = await connection.put({ user: props.user.login, password: props.user.password, new_password:password, confirm_password:confirm}, "CCPP/Login.php");
-            if(req.error) throw new Error(req.message);
+            const connection = new Connection("15", true);
+            const req: any = await connection.put({ user: props.user.login, password: props.user.password, new_password: password, confirm_password: confirm }, "CCPP/Login.php");
+            if (req.error) throw new Error(req.message);
             alert("Senha alterada com sucesso!");
             props.onClose();
         } catch (error: any) {
@@ -33,7 +33,12 @@ export default function DefaultPassword(props: {
         }
     };
     return props.open ? (
-        <div onClick={()=>props.onClose()} className='d-flex  align-items-center justify-content-center bg-black bg-opacity-25 position-absolute h-100 w-100 top-0'>
+        <div id="ModalDefaultPassword" onClick={(element: React.MouseEvent<HTMLDivElement>) => {
+            const target = element.target as HTMLElement;
+            if (target.id && target.id == "ModalDefaultPassword") {
+                props.onClose();
+            }
+        }} className='d-flex  align-items-center justify-content-center bg-black bg-opacity-25 position-absolute h-100 w-100 top-0'>
             <div className='d-flex flex-column bg-white col-8 col-sm-6 col-md-4 col-lg-3  py-4 rounded'>
                 <header className='d-flex aligm-itens-center justify-content-center my-4  w-100'>
                     <h1>Hora de trocar sua senha!</h1>
