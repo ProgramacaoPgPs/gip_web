84b898a5b1f965639302e713c61571edbf57fc2a
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2025-01-27 13:29:23 -0300

    adjusted scroll

--- src/Modules/CLPP/Chat/ChatWindow.tsx
+++ src/Modules/CLPP/Chat/ChatWindow.tsx
@@ -10,13 +10,12 @@ export default function ChatWindow(props: tChatWindow): JSX.Element {
     const { messagesContainerRef, previousScrollHeight, listMessage, page } = useWebSocket();
 
     useEffect(() => {
+        const stopPage = previousScrollHeight.current;
         setTimeout(() => {
-
             if (messagesContainerRef.current) {
-                messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight - previousScrollHeight.current;
+                messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight - stopPage;
             }
         }, 100);
-
     }, [listMessage, page]);
     return (
         <div id='divChatWindow' className='d-flex flex-column h-100 w-100'>
