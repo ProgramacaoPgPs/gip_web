e778d514b37d7263fc79420fa2927268ee739737
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2025-01-30 16:46:21 -0300

    ajusted styles SesseionTimer

--- src/Components/SessionTimer.tsx
+++ src/Components/SessionTimer.tsx
@@ -46,13 +46,18 @@ const SessionTimer: React.FC<SessionTimerProps> = ({ expirationDate }) => {
 
     // Converte milissegundos para minutos e segundos
     const formatTime = (ms: number) => {
-        const minutes = Math.floor(ms / 60000);
-        const seconds = Math.floor((ms % 60000) / 1000);
-        return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+        const hours = Math.floor(ms / 3600000); // Converte ms para horas
+        const minutes = Math.floor((ms % 3600000) / 60000); // Pega os minutos restantes
+        const seconds = Math.floor((ms % 60000) / 1000); // Pega os segundos restantes
+    
+        // Formata para sempre ter dois dígitos (ex: 01:05:09)
+        const format = (num: number) => num.toString().padStart(2, "0");
+    
+        return `${format(hours)}:${format(minutes)}:${format(seconds)}`;
     };
 
     return (
-        <div style={{width:"78px"}} className="d-flex bg-secondary flex-column position-fixed bottom-0 rounded p-1 d-flex mx-0 my-4 ">
+        <div style={{width:"88px"}} className="d-flex bg-secondary flex-column position-fixed bottom-0 rounded p-1 d-flex mx-0 my-4 ">
             <div className="d-flex justify-content-between  w-100">
                 <span className="text-white h6 m-0">{formatTime(timeLeft)}</span>
                 <i className="d-flex align-items-center fa-solid fa-clock text-white h6 m-0"></i>
