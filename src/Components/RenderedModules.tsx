d91c4d850ff5de13d3716b37ef16d767d4b55084
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2025-02-21 17:02:26 -0300

    my last commit of day

--- src/Components/RenderedModules.tsx
+++ src/Components/RenderedModules.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Clpp from '../Modules/CLPP/Clpp';
-import { useMyContext } from '../Context/MainContext';
 type Props = {
     children: JSX.Element; // Tipo para o children
 }
 export default function RenderedModules(props: Props): JSX.Element {
-    const { token } = useMyContext();
     return (
         <div className='d-flex flex-row w-100 h-100'>
             {props.children}
