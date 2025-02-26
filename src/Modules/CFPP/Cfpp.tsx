bf820a36e161e20d2588af8aa935567a25d63cb2
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2025-02-26 17:17:00 -0300

    Created module CFPP

---
+++ src/Modules/CFPP/Cfpp.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Cfpp() {
+    return (
+        <div>
+            <h1>Recursos Humanos</h1>
+        </div>
+    );
+}
