ff69f3eef6fe0d5bde4913246bfb442e6597f173
ProgramacaoPgPs <programacaopegpese@gmail.com>
2025-03-04 12:19:46 -0300

    finalizando a primeira alteração

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -2,8 +2,6 @@ import { Store } from "react-notifications-component";
 import { Connection } from "../Connection/Connection";
 import { iReqConn } from "../Interface/iConnection";
 import Translator from "./Translate";
-import { DateConverter } from "../Modules/GTPP/Class/DataConvert";
-import { ITask } from "../Interface/iGIPP";
 
 const connection = new Connection("18", true);
 
@@ -17,46 +15,6 @@ export const convertdate = (date: string): string | null => {
     }
 
     return parsedDate.toLocaleDateString('pt-BR');
-};
-
-export const filterTasks = (
-    tasks: ITask[],
-    searchTerm: string = "",
-    rangeDateInitial: string = "",
-    rangeDateInitialFinal: string = "",
-    rangeDateFinal: string = "",
-    rangeDateFinalFinal: string = "",
-    priority: Number,
-    IdentityDataUser: Number,
-    user_id: any,
-) => {
-    if (!searchTerm && !rangeDateInitial && !rangeDateFinal && priority === 3 && IdentityDataUser === 3 ) return tasks; // && !IdentityDataUser
-
-    const filter = tasks
-        .filter(task => !searchTerm || task.description.toUpperCase().includes(searchTerm.toUpperCase()))
-        .filter(task => priority === 3 || task.priority === priority)
-        .filter(task => IdentityDataUser === 3 ? task : IdentityDataUser === 2 ? task.user_id !== user_id.id : task.user_id === user_id.id)
-        .filter(task => {
-            if(rangeDateInitialFinal && rangeDateInitial){
-                if(rangeDateFinal && rangeDateFinalFinal) {
-                    return task.initial_date  >= rangeDateFinal && task.final_date <= rangeDateFinalFinal;
-                } 
-                return task.initial_date >= rangeDateInitial && task.final_date <=rangeDateInitialFinal;
-            }else {
-                return task;
-            }            
-        })
-        .filter(task => {
-            if(rangeDateFinal && rangeDateFinalFinal) {
-                if(rangeDateInitial && rangeDateInitialFinal) {
-                    return task.initial_date >= rangeDateInitial && task.initial_date <=rangeDateInitialFinal;
-                }
-                return task.final_date >= rangeDateFinal && task.final_date <= rangeDateFinalFinal;
-            } else {
-                return task;
-            }
-        })
-        return filter;
 };
 
 export const httpGet = async (url: string, params: any = {}) => {
