01e2f31cd34a8eb1b94a6d6fb234d8acde672e7b
ProgramacaoPgPs <programacaopegpese@gmail.com>
2025-03-05 10:52:28 -0300

    padronizando para o KISS as funções

--- src/Modules/GTPP/ComponentsCard/ColumnTask/filtercardTask.tsx
+++ src/Modules/GTPP/ComponentsCard/ColumnTask/filtercardTask.tsx
@@ -1,19 +1,48 @@
 import { ITask } from "../../../../Interface/iGIPP";
 
-export const filterTasks = ( tasks: ITask[], searchTerm: string = "", rangeDateInitial: string = "", rangeDateInitialFinal: string = "", rangeDateFinal: string = "", rangeDateFinalFinal: string = "", priority: Number, IdentityDataUser: Number, user_id: any) => {
+export const filterTasks = ( tasks: ITask[], searchTerm: String = "", rangeDateInitial: String = "", rangeDateInitialFinal: String = "", rangeDateFinal: String = "", rangeDateFinalFinal: String = "", priority: Number, IdentityDataUser: Number, user_id: any) => {
     if (!searchTerm && !rangeDateInitial && !rangeDateFinal && priority === 3 && IdentityDataUser === 3 ) return tasks;
     const filter = tasks
-        .filter(task => !searchTerm || task.description.toUpperCase().includes(searchTerm.toUpperCase()))
-        .filter(task => priority === 3 || task.priority === priority)
-        .filter(task => IdentityDataUser === 3 ? task : IdentityDataUser === 2 ? task.user_id !== user_id.id : task.user_id === user_id.id)
-        .filter(task => (rangeDateInitialFinal && rangeDateInitial) ?
-                   (rangeDateFinal && rangeDateFinalFinal) ? 
-                        task.initial_date  >= rangeDateFinal && task.final_date <= rangeDateFinalFinal :
-                        task.initial_date >= rangeDateInitial && task.final_date <=rangeDateInitialFinal : 
-                        task)
-        .filter(task => (rangeDateFinal && rangeDateFinalFinal) ?
-                   (rangeDateInitial && rangeDateInitialFinal) ? 
-                        task.initial_date >= rangeDateInitial && task.initial_date <=rangeDateInitialFinal :
-                        task.final_date >= rangeDateFinal && task.final_date <= rangeDateFinalFinal : task)
+        .filter(task => searchTask(task, searchTerm))
+        .filter(task => priorityTask(task, priority))
+        .filter(task => userIndentity(task, IdentityDataUser, user_id))
+        .filter(task => dateInitial(task, rangeDateInitial, rangeDateInitialFinal, rangeDateFinal, rangeDateFinalFinal))
+        .filter(task => dateFinal(task, rangeDateFinal, rangeDateFinalFinal, rangeDateInitial, rangeDateInitialFinal))
         return filter;
 };
+
+// data inicial
+function dateInitial(task: ITask, rangeDateInitial:String, rangeDateInitialFinal:String, rangeDateFinal:String, rangeDateFinalFinal:String) {
+    return (rangeDateInitialFinal && rangeDateInitial) ?
+                (rangeDateFinal && rangeDateFinalFinal) ? 
+                    task.initial_date  >= rangeDateFinal && task.final_date <= rangeDateFinalFinal :
+                    task.initial_date >= rangeDateInitial && task.final_date <=rangeDateInitialFinal : 
+                    task;
+}
+
+// Data final
+function dateFinal(task: ITask, rangeDateFinal:String, rangeDateFinalFinal:String, rangeDateInitial:String, rangeDateInitialFinal:String) {
+    return (rangeDateFinal && rangeDateFinalFinal) ?
+                (rangeDateInitial && rangeDateInitialFinal) ? 
+                    task.initial_date >= rangeDateInitial && task.initial_date <=rangeDateInitialFinal :
+                    task.final_date >= rangeDateFinal && task.final_date <= rangeDateFinalFinal : 
+                    task;
+}
+
+
+// Identificando as tarefas de colaboração e se é de usuário.
+function userIndentity(task:ITask, IdentityDataUser: Number, user_id: {id: Number}) {
+    return IdentityDataUser === 3 ? task : IdentityDataUser === 2 ? task.user_id !== user_id.id : task.user_id === user_id.id;
+}
+
+// priopridade da tarefa
+function priorityTask(task: ITask, priority:Number) {
+    return priority === 3 || task.priority === priority;
+}
+
+
+// pesquisando a tarefa!
+function searchTask(task: ITask, searchTerm: String) {
+    return !searchTerm || task.description.toUpperCase().includes(searchTerm.toUpperCase())
+}
+
